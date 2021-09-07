dfbcaff0fcee009e25719173b608aab27d495f00
skuttenkuler <sam.kuttenk@gmail.com>
2021-09-06 19:13:23 -0500

    Initial Commit

---
+++ Shadow Airlines/Shadow_AirlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shadow_AirlinesApp.swift
+//  Shadow Airlines
+//
+//  Created by Sam Kuttenkuler on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Shadow_AirlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
