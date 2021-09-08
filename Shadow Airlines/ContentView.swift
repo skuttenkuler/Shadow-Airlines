93bb23e221d9e9c8060b3e44822cdf780a0726da
skuttenkuler <sam.kuttenk@gmail.com>
2021-09-08 16:38:41 -0500

    Home screen styled and new icon added

--- Shadow Airlines/ContentView.swift
+++ Shadow Airlines/ContentView.swift
@@ -17,12 +17,7 @@ struct HomeView: View {
                 
             HomeContent()
 
-            }.background(
-                Image("background")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all)
-                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-            )
+            }.background(LinearGradient(gradient: Gradient(colors: [.red, .black]), startPoint: .top, endPoint: .bottom).frame(width: UIScreen.main.bounds.width, height:1000))
     
         }
     }
@@ -32,19 +27,23 @@ struct HomeContent: View {
     //view block
     
     var body: some View {
-        Text("Shadow Airways")
-            .font(.system(size: 40))
+        Text("Shadow Airlines")
+            .font(.system(size: 60))
             .bold()
             .italic()
             .offset(y: -25)
+            .foregroundColor(Color.white)
+            .shadow(color: .black, radius: 4, x: 10, y: 10)
         Spacer()
-        
+        Image("cards").resizable()
         NavigationLink(destination:BookTicketView(), label: {
-            Text("Book Flight")
+            Text("Begin")
                 .font(.system(size:60))
+                .frame(width: 200, height: 100)
                 .foregroundColor(Color.white)
-                .background(Color.blue)
+                .background(Color(red: 203 / 255,green: 33 / 255 ,blue: 39 / 255))
                 .cornerRadius(20)
+                .shadow(color: .black, radius: 2, x: 10, y: 10)
                 
             })
 }
