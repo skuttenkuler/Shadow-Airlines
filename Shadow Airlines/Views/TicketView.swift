dfef3808535acf26cfb360ec6b2d81024698d924
skuttenkuler <sam.kuttenk@gmail.com>
2021-09-09 15:18:21 -0500

    new image, snapshot taking correct frame, printing image, need to make tiny tweaks

--- Shadow Airlines/Views/TicketView.swift
+++ Shadow Airlines/Views/TicketView.swift
@@ -17,34 +17,35 @@ struct TicketView: View {
         ZStack{
             VStack{
             Text("Nashville (BNA)")
-                .offset(x: 20,y: -160)
-                .font(.system(size:23)
+                .offset(x: 20,y: -170)
+                .font(.system(size:25)
                 .weight(.heavy))
                 .foregroundColor(Color.black)
                 .frame(minWidth: 300, idealWidth: 300, maxWidth: 300, minHeight: 100, idealHeight: 100, maxHeight: 100)
                 Text(destination)
                     .offset(x: 30,y: -200)
+                    .font(.system(size:25)
+                    .weight(.heavy))
+                    .foregroundColor(Color.black)
+                    .frame(minWidth: 350, idealWidth: 300, maxWidth: 350, minHeight: 100, idealHeight: 100, maxHeight: 100)
+                
+                Text("\(departure, style: .date)")
+                    .offset(x: 75,y: 28)
                     .font(.system(size:23)
                     .weight(.heavy))
                     .foregroundColor(Color.black)
-                    .frame(minWidth: 300, idealWidth: 300, maxWidth: 300, minHeight: 100, idealHeight: 100, maxHeight: 100)
-                
-                Text("\(departure, style: .date)")
-                    .offset(x: 60,y: 65)
-                    .font(.system(size:19)
-                    .weight(.heavy))
-                    .foregroundColor(Color.black)
                     .frame(width:250)
-                Text("\(departure, style: .time)").offset(x: -75,y: 150).font(.system(size:25).weight(.heavy)).foregroundColor(Color.black)
+                Text("\(departure, style: .time)").offset(x: -120,y: 60).font(.system(size:25).weight(.heavy)).foregroundColor(Color.black)
         Button("Book"){
             let image = snapshot()
             UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
-        }.offset(x: 90 , y:80).font(.system(size:30)).foregroundColor(Color.white)
+        }.offset(x: 120 , y:0).font(.system(size:30)).foregroundColor(Color.white)
     }.background(
-        Image("Ticket2")
+        Image("S_Ticket")
             .resizable()
             .edgesIgnoringSafeArea(.all)
-            .frame(width:400, height: 680)
+            .frame(width:400, height: 590)
+            .offset(x: 0,y: -40)
     )}
 }
 }
